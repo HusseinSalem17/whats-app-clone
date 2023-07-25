a991541cb1cc320d02b433f2413a77401a794b76
hussein-salemm <husseinsalem177@gmail.com>
2023-07-25 17:58:09 +0300

    selectContact done

--- lib/features/select_contacts/repository/select_contact_repository.dart
+++ lib/features/select_contacts/repository/select_contact_repository.dart
@@ -1,7 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_contacts/flutter_contacts.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:whatsapp_ui/core/utils/utils.dart';
+import 'package:whatsapp_ui/models/user_model.dart';
 
 final selectContactsRepositoryProvider = Provider(
   (ref) => SelectContactRepository(
@@ -24,5 +27,19 @@ class SelectContactRepository {
       debugPrint(e.toString());
     }
     return contacts;
+  }
+
+  void selectContact(Contact selectedContact, BuildContext context) async {
+    try {
+      var userCollection = await firestore.collection('users').get();
+      bool isFound = false;
+
+      for (var document in userCollection.docs) {
+        var userData = UserModel.fromMap(document.data());
+        print(selectedContact.phones[0].number);
+      }
+    } catch (e) {
+      showSnackBar(context: context, content: e.toString());
+    }
   }
 }
