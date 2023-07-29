0d05190b5225bca0468506dcbd3e49ab9266dac1
hussein-salemm <husseinsalem177@gmail.com>
2023-07-29 22:39:52 +0300

    Uploading Status/Stories

--- lib/router.dart
+++ lib/router.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:whatsapp_ui/core/widgets/error.dart';
 import 'package:whatsapp_ui/features/auth/screens/login_screen.dart';
@@ -5,6 +7,8 @@ import 'package:whatsapp_ui/features/auth/screens/otp_screen.dart';
 import 'package:whatsapp_ui/features/select_contacts/screens/select_contacts_screen.dart';
 import 'package:whatsapp_ui/features/chat/screens/mobile_chat_screen.dart';
 import 'package:whatsapp_ui/screens/user_information_screen.dart';
+
+import 'features/status/screens/confirm_status_screen.dart';
 
 Route<dynamic> generateRoute(RouteSettings settings) {
   switch (settings.name) {
@@ -31,6 +35,13 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       final uid = arguments['uid'];
       return MaterialPageRoute(
         builder: (context) => MobileChatScreen(name: name, uid: uid),
+      );
+    case ConfirmStatusScreen.routeName:
+      final file = settings.arguments as File;
+      return MaterialPageRoute(
+        builder: (context) => ConfirmStatusScreen(
+          file: file,
+        ),
       );
     default:
       return MaterialPageRoute(
